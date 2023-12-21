1b74c6ce0ff34b142186b6ebf123e3385762fc86
Ghaniyat27 <ganiyat4real1@gmail.com>
2023-12-20 17:31:24 -0800

    class Square that defines a square by size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 # 2-square.py
+"""Square class defination."""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """Square class body"""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """Square class constructor
+        Args:
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size = size
