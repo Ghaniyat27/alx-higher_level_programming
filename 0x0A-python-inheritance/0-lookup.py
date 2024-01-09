f327bee791773a7e2e56aaa0555263ee114bfee4
Ghaniyat27 <ganiyat4real1@gmail.com>
2024-01-08 18:12:29 -0800

    0x0A-python-inheritance task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-lookup.py
+"""Defines available attributes/methods of an object."""
+
+
+def lookup(obj):
+    """Return a list of available attributes."""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+"""Empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """BaseGeometry class body."""
+    pass
