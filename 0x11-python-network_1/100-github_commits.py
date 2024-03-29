3770e83cafc6a7cc93d8d495821a018f3ec33af2
Ghaniyat27 <ganiyat4real1@gmail.com>
2024-03-28 19:31:29 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-Python script that list 10 commits (from the most recent to oldest) of a repository
-in GitHub.
+"""lists the 10 most recent commits on a given GitHub repository.
 """
 import sys
 import requests
 
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
